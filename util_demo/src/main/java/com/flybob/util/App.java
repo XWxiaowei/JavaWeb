45c96abb3315feb33ab825aa9a810c0fa7dce845
FlyBob <2809641033@qq.com>
2017-05-29 22:25:21 +0800

    新建工具类

---
+++ util_demo/src/main/java/com/flybob/util/App.java
@@ -0,0 +1,13 @@
+package com.flybob.util;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
