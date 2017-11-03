a6593659c425dedb9e91b123971461c17567ff43
xiangwei-rt <2809641033@qq.com>
2017-11-03 20:53:25 +0800

    SSH的demo，添加单元测试

--- ssh-demo/src/main/java/com/jay/ssh/util/HttpUtil.java
+++ ssh-demo/src/main/java/com/jay/ssh/util/HttpUtil.java
@@ -8,12 +8,6 @@ import org.apache.commons.httpclient.methods.GetMethod;
 import org.apache.commons.httpclient.methods.PostMethod;
 import org.apache.commons.httpclient.params.HttpMethodParams;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.net.URL;
-import java.net.URLConnection;
 import java.util.Map;
 
 
@@ -23,54 +17,7 @@ public class HttpUtil {
 
 	private final static int CONNECT_TIMEOUT = 5000; // in milliseconds
 	 private final static String DEFAULT_ENCODING = "UTF-8";
-	 
-	 public static String postData(String urlStr, String data){
-	        return postData(urlStr, data, null);
-	 }
-	 
-	 public static String postData(String urlStr, String data, String contentType) {
-		 BufferedReader reader = null;
-		 try {
-	            URL url = new URL(urlStr);
-	            URLConnection conn = url.openConnection();
-	            conn.setDoOutput(true);
-	            conn.setConnectTimeout(CONNECT_TIMEOUT);
-	            conn.setReadTimeout(CONNECT_TIMEOUT);
-	            if(contentType !=null)
-	                conn.setRequestProperty("content-type", contentType);
-	            OutputStreamWriter writer = new OutputStreamWriter(conn.getOutputStream(), DEFAULT_ENCODING);
-	            if(data == null){
-	            	 writer.write("");
-	            }else{
-	            	writer.write(data); 
-	            }
-	            writer.flush();
-	            writer.close();  
-	 
-	            reader = new BufferedReader(new InputStreamReader(conn.getInputStream(), DEFAULT_ENCODING));
-	            StringBuilder sb = new StringBuilder();
-	            String line = null;
-	            while ((line = reader.readLine()) != null) {
-	                sb.append(line);
-	                sb.append("\r\n");
-	            }
-	            return sb.toString();
-	        }catch (IOException e) {
-	            e.printStackTrace();
-	            log.error(e.getMessage());
-	        } finally {
-	            try {
-	                if (reader != null)
-	                    reader.close();
-	            } catch (IOException e) {
-	            	log.error(e.getMessage());
-	            }
-	        }
-	        return null;
-	 }
-	 
-	 
-	 
+
 	 public static String  postHttp(String url,Map<String,String> map){
 	        HttpClient httpclient = null;
 	        PostMethod post = null;
--- ssh-demo/src/test/com/jay/ssh/service/impl/MyInvoiceServiceImplTest.java
+++ ssh-demo/src/test/com/jay/ssh/service/impl/MyInvoiceServiceImplTest.java
@@ -14,6 +14,10 @@ import org.powermock.modules.junit4.PowerMockRunner;
 
 import java.util.List;
 
+import static org.mockito.Matchers.anyMap;
+import static org.mockito.Matchers.anyString;
+
+
 /**
  * @author xiang.wei
  * @create 2017/10/26 17:08
@@ -23,7 +27,6 @@ import java.util.List;
 public class MyInvoiceServiceImplTest {
     @InjectMocks
     private MyInvoiceService myInvoiceService = new MyInvoiceServiceImpl();
-    HttpUtil mockHttp;
     @Before
     public void setUp(){
         PowerMockito.mockStatic(HttpUtil.class);
@@ -36,18 +39,27 @@ public class MyInvoiceServiceImplTest {
                 "\"invoiceType\":\"0\",\"isDefault\":0},{\"addDate\":1509092635000,\"" +
                 "buyerId\":\"9E59A2D27B7748848FB65041B854240E\",\"editDate\":1509094177000,\"headName\":\"项伟测试二\",\"headType\":\"0\",\"invoiceId\":\"720CF6C50E594283B01C79D03D6D52B2\"" +
                 ",\"invoiceType\":\"0\",\"isDefault\":1}],\"msg\":\"成功\",\"code\":104}";
-        PowerMockito.when(HttpUtil.getHttp("")).thenReturn(result_http);
 //      1、  buyerId为空
         String buyerId = null;
         Assert.assertEquals(null, myInvoiceService.getMyInvoice(buyerId));
 //        2、buyerId不为空
         buyerId = "FF8080810F5E601526";
+        PowerMockito.when(HttpUtil.getHttp(anyString())).thenReturn(result_http);
         List<MyInvoice> result = myInvoiceService.getMyInvoice(buyerId);
-        Assert.assertEquals(3,result.size());
+        Assert.assertEquals(2,result.size());
     }
 
     @Test
     public void testSaveMyInvoice() throws Exception {
+        MyInvoice myInvoice = new MyInvoice();
+        myInvoice.setBuyerId("1111");
+        myInvoice.setInvoiceType("0");
+        myInvoice.setIsDefault("0");
+        myInvoice.setHeadName("项伟测试");
+        String result_http = "{\"data\":\"000000003DD0690913c\",\"msg\":\"成功\",\"code\":104}";
+        PowerMockito.when(HttpUtil.postHttp(anyString(),anyMap())).thenReturn(result_http);
+        Assert.assertEquals(myInvoiceService.saveMyInvoice(myInvoice),"000000003DD0690913c");
+
     }
 
 }
