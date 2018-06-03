1a94fffa78cd50d14b38f77023aadc610e21a9b9
XWxiaowei <2809641033@qq.com>
2018-06-03 14:53:47 +0800

    request对象线程安全的测试

---
+++ ssm-demo/src/main/java/com/jay/ssm/controller/TestController1.java
@@ -0,0 +1,33 @@
+package com.jay.ssm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by xiang.wei on 2018/6/3
+ *
+ * @author xiang.wei
+ */
+@Controller
+public class TestController1 {
+
+    public static Set<String> set = new HashSet<>();
+
+    @RequestMapping("/test.do")
+    public void  test(HttpServletRequest request) throws InterruptedException {
+//        判断线程安全
+        String value = request.getParameter("key");
+        if (set.contains(value)) {
+            System.out.println(value + "\t重复出现，request不安全");
+        } else {
+            System.out.println(value);
+            set.add(value);
+        }
+
+        Thread.sleep(1000);
+    }
+}
---
+++ ssm-demo/src/main/java/com/jay/ssm/controller/TestController2.java
@@ -0,0 +1,13 @@
+package com.jay.ssm.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by xiang.wei on 2018/6/3
+ *
+ * @author xiang.wei
+ */
+@Controller
+public class TestController2 {
+
+}
